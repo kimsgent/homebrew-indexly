7d63720788f5899fe2ed849d4721ee9de02fd04d
kimsgent <gentkims@gmail.com>
2026-01-20 22:11:51 +0100

    fix(brew): update formula for v1.2.0 with correct SHA and trailing lines chore(release): prepare documentation and Homebrew tap for v1.2.0

--- Formula/indexly.rb
+++ Formula/indexly.rb
@@ -1,8 +1,8 @@
 class Indexly < Formula
   desc "Local semantic file indexing and search tool"
   homepage "https://github.com/kimsgent/project-indexly"
-  url "https://github.com/kimsgent/project-indexly/archive/refs/tags/v1.1.4.tar.gz"
-  sha256 "ad8bea8d51fc6d581a6d43110644ac09ff33ecc8d4fc2f527e2d2ef32fd77542"
+  url "https://github.com/kimsgent/project-indexly/archive/refs/tags/v1.2.0.tar.gz"
+  sha256 "ceddf70869d7b8aadfcabab0589b9db966e414a571e5af660a44d9c4b7e19a3e"
   license "MIT"
 
   depends_on "python@3.11"
@@ -17,41 +17,8 @@ class Indexly < Formula
     bin.install_symlink libexec/"bin/indexly"
   end
 
-  def caveats
-    <<~EOS
-      Indexly is installed successfully.
-
-      If you encounter runtime issues, add the following to your shell config.
-
-      ---- Bash (.bashrc) ----
-      echo 'export PATH="$(brew --prefix)/opt/python@3.11/bin:$PATH"' >> ~/.bashrc
-      echo 'export PYTHONPATH="$(brew --prefix)/Cellar/indexly/#{version}/libexec:$PYTHONPATH"' >> ~/.bashrc
-      echo '
-      indexly() {
-        PYTHONPATH="$(brew --prefix)/Cellar/indexly/#{version}/libexec/lib/python3.11/site-packages:$PYTHONPATH" \\
-        "$(brew --prefix)/opt/python@3.11/bin/python3.11" \\
-        "$(brew --prefix)/Cellar/indexly/#{version}/libexec/bin/indexly" "$@"
-      }
-      ' >> ~/.bashrc
-
-      ---- Zsh (.zshrc) ----
-      echo 'export PATH="$(brew --prefix)/opt/python@3.11/bin:$PATH"' >> ~/.zshrc
-      echo 'export PYTHONPATH="$(brew --prefix)/Cellar/indexly/#{version}/libexec:$PYTHONPATH"' >> ~/.zshrc
-      echo '
-      indexly() {
-        PYTHONPATH="$(brew --prefix)/Cellar/indexly/#{version}/libexec/lib/python3.11/site-packages:$PYTHONPATH" \\
-        "$(brew --prefix)/opt/python@3.11/bin/python3.11" \\
-        "$(brew --prefix)/Cellar/indexly/#{version}/libexec/bin/indexly" "$@"
-      }
-      ' >> ~/.zshrc
-
-      Reload your shell after applying.
-    EOS
-  end
-
   test do
     system bin/"indexly", "--version"
     system bin/"indexly", "--help"
   end
 end
-
